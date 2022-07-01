713881fe8f7800e6feae5af5eafd400f1d06fdec
CagkanCaglar <cagkancaglar@hotmail.com>
2022-07-02 00:11:54 +0300

    update

--- script.js
+++ script.js
@@ -7,5 +7,12 @@ $(document).ready(function(){
   function myFunction(x) {
     x.classList.toggle("change");
   }
+
+  const express = require('express');
+const app = express();
+
+app.get('/', function(request, response){
+    response.sendFile('index.html');
+});
   
   
