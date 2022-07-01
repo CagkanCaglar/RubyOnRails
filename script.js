9d64678849761fca138cef3bad7544895f44cf01
CagkanCaglar <cagkancaglar@hotmail.com>
2022-07-02 01:11:42 +0300

    update

--- script.js
+++ script.js
@@ -15,23 +15,6 @@ var path = require('path');
 
 app.use(express.static(path.join(__dirname, '/')));
 
-
-// app.get('/json', function(req, res) {
-//     console.log("GET the json");
-//     res
-//         .status(200)
-//         .json( {"jsonData" : true} );
-// });
-
-// app.get('/file', function(req, res) {
-//     console.log("GET the file");
-//     res
-//         .status(200)
-//         .sendFile(path.join(__dirname, 'script.js'));
-// });
-
-
-
 const server = app.listen(process.env.PORT || 5000, () => {
   const port = server.address().port;
   console.log(`Express is working on port ${port}`);
