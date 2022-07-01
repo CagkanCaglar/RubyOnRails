eb17602c3119bc01dddc8bd5d62e9c0d4bdc4421
CagkanCaglar <cagkancaglar@hotmail.com>
2022-07-02 00:35:28 +0300

    update

--- script.js
+++ script.js
@@ -14,8 +14,31 @@ $(document).ready(function(){
   }
 
 
-  app.use(express.static('./'));
+var express = require('express');
+var app = express();
+var path = require('path');
 
-  app.get('/', function(req, res) { 
-      res.render('index.html');
-  });
+
+app.use(express.static(path.join(__dirname, '/')));
+
+
+app.get('/json', function(req, res) {
+    console.log("GET the json");
+    res
+        .status(200)
+        .json( {"jsonData" : true} );
+});
+
+app.get('/file', function(req, res) {
+    console.log("GET the file");
+    res
+        .status(200)
+        .sendFile(path.join(__dirname, 'app.js'));
+});
+
+
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  const port = server.address().port;
+  console.log(`Express is working on port ${port}`);
+});
