1b1b50389bcb6238cd90a2473775371c8854c197
CagkanCaglar <cagkancaglar@hotmail.com>
2022-07-01 23:29:39 +0300

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".menu-icon").click(function(){
+      $(".mobil-menu-container").toggleClass("mobile-menu-show");
+    });
+  });
+  
+  function myFunction(x) {
+    x.classList.toggle("change");
+  }
+  
+  
